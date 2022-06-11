5e247201ac62004ec47c2bef135c640df77e4422
gregory <smartis2816@gmail.com>
2022-06-11 19:00:45 +0300

    1st task completed

--- Seminar 2/Homework2.cs
+++ Seminar 2/Homework2.cs
@@ -1,4 +1,10 @@
 
+// TASK1
+// Option 1 не запрашивает у пользователя ввод числа повторно в случае ошибки, а Option 2 - запрашивает
+
+
+// Option 1
+/*
 int FindSecondPart(int num)
 {
     if(num >= 100 && num <= 999)
@@ -21,3 +27,23 @@ else
 {
     Console.WriteLine($"Second part of {number} is {result}");
 }
+*/
+
+
+// Option 2
+/*
+int FindSecondPart()
+{
+    Console.Write("Input three-digit number: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    while(number < 100 ^ number > 999)
+    {
+        Console.WriteLine("Your number is not three-digit");
+        Console.Write("Input three-digit number: ");
+        number = Convert.ToInt32(Console.ReadLine());
+    }
+    return (number / 10) % 10;
+}
+int result = FindSecondPart();
+Console.WriteLine($"Second part of your number is {result}");
+*/
