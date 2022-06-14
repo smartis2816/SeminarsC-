8a0985670e4fc8e01407077ab62973d5b6189d64
gregory <smartis2816@gmail.com>
2022-06-14 19:42:16 +0300

    Homework completed

--- Seminar3/Homework3.cs
+++ Seminar3/Homework3.cs
@@ -1,4 +1,24 @@
+// TASK 1
+/*
+bool PalindromeOrNot(int number)
+{
+    return (number / 10000 == number % 10 && (number / 1000) % 10 == (number % 100) / 10);
+}
 
+Console.Write("Enter any five-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while(number < 10000 && number > -10000 ^ number > 99999 ^ number < -99999)
+{
+    Console.Write("You entered not five-digit number. Please try again: ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+
+if(PalindromeOrNot(number)) Console.WriteLine("This number is palindrome");
+else Console.WriteLine("This number is not palindrome");
+*/
+// TASK 2
+/*
 double FindLengthIn3D (double xA, double yA, double zA, double xB, double yB, double zB)
 {
     return Math.Sqrt(Math.Pow((xB-xA), 2) + Math.Pow((yB - yA), 2) + Math.Pow((zB - zA), 2));
@@ -21,4 +41,29 @@ Console.Write("zB = ");
 double zB = Convert.ToDouble(Console.ReadLine());
 
 double length = FindLengthIn3D(xA, yA, zA, xB, yB, zB);
-Console.WriteLine($"Length between point A and Point B in 3D space is {length}");
+Console.WriteLine($"Length between point A and point B in 3D space is {length}");
+*/
+
+// TASK 3
+/*
+void Cube (int N)
+{
+    int current = 1;
+    while(current <= N)
+    {
+        Console.Write(Math.Pow((current), 3) + " ");
+        current ++;
+    }
+
+}
+
+Console.Write("Enter natural number N: ");
+int N = Convert.ToInt32(Console.ReadLine()); 
+while(N < 1)
+{
+    Console.Write("This program works only with natural nymbers. Please enter number N again: ");
+    N = Convert.ToInt32(Console.ReadLine());
+}
+
+Cube(N);
+*/
