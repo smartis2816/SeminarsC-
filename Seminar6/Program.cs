0b2f711a291a38798e437bdfdc90ef8153df6e10
gregory <smartis2816@gmail.com>
2022-06-23 19:44:30 +0300

    Seminar 6

--- Seminar6/Program.cs
+++ Seminar6/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача на разворот массива
+int [] ReverseArray(int[] array)
+{
+    int temp;
+    int j = array.Length - 1;
+
+    for(int i = 0; i < array.Length / 2; i++, j--)
+    {
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    return array;
+}
+/*
+// Альтернативная запись через while
+int [] ReverseArray(int[] array)
+{
+    int temp;
+    int j = array.Length - 1, i = 0;
+
+    while(i < j)
+    {
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+
+        i++;
+        j++;
+    }
+
+    return array;
+}
+*/
+
+int[] myArray = {2, 4, 6, 8};
+myArray = ReverseArray(myArray);
+
+for(int i = 0; i < myArray.Length; i++)
+{
+    Console.Write(myArray[i] + " ");
+}
