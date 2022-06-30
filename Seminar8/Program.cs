ae32ea0c4590bbd3d3f2c486304d2064e15162dc
gregory <smartis2816@gmail.com>
2022-06-30 19:14:30 +0300

    Seminar 8

--- Seminar8/Program.cs
+++ Seminar8/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача №1.
 // Задайте двумерный массив. 
 // Напишите программу, которая поменяет местами первую и последнюю строку массива.
-
+/*
 int[,] CreateTwoDimArray(int m, int n, int min, int max)
 {
     int[,] array = new int[m, n];
@@ -37,7 +37,7 @@ int[,] ChangeLines(int[,] array, int strIndex1, int strIndex2)
 
     return array;
 }
-
+*/
 /*
 int[,] myArray = CreateTwoDimArray(4, 5, 1, 9);
 ShowArray(myArray);
@@ -49,10 +49,76 @@ ShowArray(myArray);
 // Задача №2.
 // Напишите программу, которая заменяет строки на столбцы. 
 // В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+/*
+int[,] ChangeRowsColumns(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0) - 1; i++)
+    {
+        for (int j = i + 1; j < array.GetLength(1); j++)
+        {
+            int temp = array[i, j];
+            array[i, j] = array[j, i];
+            array[j, i] = temp;
+        }
+    }
+    return array;
+}
+*/
+/*
+int m, n, min, max;
 
+do
+{
+    Console.WriteLine("Число строк и столбцов должно быть одинаковым. Введите данные: ");
+    m = Convert.ToInt32(Console.ReadLine());
+    n = Convert.ToInt32(Console.ReadLine());
+    min = Convert.ToInt32(Console.ReadLine());
+    max = Convert.ToInt32(Console.ReadLine());
+}
+while (m != n); // Выполнять, пока m не равно n
 
+int[,] myArray = CreateTwoDimArray(m, n, min, max);
+ShowArray(myArray);
+Console.WriteLine();
+myArray = ChangeRowsColumns(myArray);
+ShowArray(myArray);
+*/
 // Задача №3.
 // Из двумерного массива целых чисел удалить строку и столбец,
 // на пересечении которых расположен наименьший элемент (записать туда 0).
+/*
+int[,] DeleteRowColumn(int[,] array)
+{
 
+    int minRow = 0;
+    int minColumn = 0;
+    int min = array[minRow, minColumn];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] < min)
+            {
+                min = array[i, j];
+                minRow = i;
+                minColumn = j;
+            }
+        }
+    }
 
+    for (int i = 0; i < array.GetLength(0); i++)
+        array[i, minColumn] = 0;
+
+    for (int j = 0; j < array.GetLength(1); j++)
+        array[minRow, j] = 0;
+
+    return array;
+}
+*/
+/*
+int[,] myArray = CreateTwoDimArray(4,6,1,9);
+ShowArray(myArray);
+Console.WriteLine();
+myArray = DeleteRowColumn(myArray);
+ShowArray(myArray);
+*/
