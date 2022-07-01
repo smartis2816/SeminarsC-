54ed570e122e54b1386fddcf506ee9ba6846ef91
gregory <smartis2816@gmail.com>
2022-07-01 16:55:21 +0300

    67%

--- Seminar8/Homework8.cs
+++ Seminar8/Homework8.cs
@@ -59,7 +59,7 @@ int[,] AscendingRows(int[,] array)
 
     return array;
 }
-
+/*
 Console.Write("Rows. ");
 int m = RowsOrColumnsRequest();
 Console.Write("Columns. ");
@@ -73,3 +73,85 @@ ShowTwoDimArray(newArrayTask1);
 AscendingRows(newArrayTask1);
 Console.WriteLine("Modified array.");
 ShowTwoDimArray(newArrayTask1);
+*/
+
+// TASK 2
+// Метод для запроса размера матрицы и проверки введённых чисел
+int[] RectangularArrayRequest()
+{
+    int N, M;
+    int[] array = new int[2];
+    do
+    {
+        Console.WriteLine("Matrix must be Rectangular. Quantity of rows and columns must be different.");
+        Console.WriteLine("Input numbers: ");
+        N = Convert.ToInt32(Console.ReadLine());
+        M = Convert.ToInt32(Console.ReadLine());
+    }
+    while(N < 1 || M < 1 || N == M);
+    array[0] = N;
+    array[1] = M;
+    return array;
+}
+// Находит суммы элементов строк и записывает в массив
+int[] FindSumsRows(int[,] array)
+{
+    
+    int[] Sums = new int[array.GetLength(0)];
+    int k = 0;
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i,j];
+            Sums[k] = sum;
+        }
+        k++;
+    }
+    return Sums;
+}
+// Находит элемент с минимальным значением в массиве
+int FindRowWithMinSum(int[] array)
+{
+    int min = 0;
+    for(int i = 1; i < array.Length; i++)
+    {
+        if(array[min] > array[i]) min = i;
+    }
+    
+    return min;
+}
+// Выводит на экран двумерный массив + суммы строк + строку с мин. суммой   
+void ShowArraySumsAndMinRow(int[,] array, int[] array2, int minRow)
+{
+    int k = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write($"Row №{k + 1}: ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine(" ->  The sum of the row is " + array2[k]);
+        k++;
+    }
+    Console.WriteLine();
+    Console.WriteLine($"The sequence number of the row with the minimum sum is {minRow + 1}");
+    Console.WriteLine();
+}
+
+int[] Indexes = RectangularArrayRequest();
+int m = Indexes[0];
+int n = Indexes[1];
+Console.WriteLine("Enter minimum and maximum possible values of numbers in array: ");
+int min = Convert.ToInt32(Console.ReadLine());
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] newArrayTask2 = CreateRandomTwoDemArray(m, n, min, max);
+ShowTwoDimArray(newArrayTask2);
+int[] SumsOfRows = FindSumsRows(newArrayTask2);
+
+int minRow = FindRowWithMinSum(SumsOfRows);
+ShowArraySumsAndMinRow(newArrayTask2, SumsOfRows, minRow);
+
